ca3c2437d06df5c00ea77c43b23932de8aaa4ee2
Minngc <ming109152@gmail.com>
2023-04-11 20:53:08 +0800

    global

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,51 @@
-import { Input } from "@/components/input/inedx";
-import { Button } from "@/components/button";
+import { Input } from "@/components/input";
+import { Button, NextButton } from "@/components/button";
 import { A } from "@/components/a";
+import { AccountItem } from "@/components/accountItem";
+import { GithubIcon, QqIcon, MsIcon } from "@/components/icon";
+import classNames from "classnames";
+import styles from "@/styles/Home.module.scss";
+import { Form } from "@/components/form";
+import Link from "next/link";
+import BackLayout from "@/components/Layout/BackLayout";
 
 export default function Home() {
   return (
     <>
-      <Input defaultValue={"123"} />
-      <Button>登录</Button>
-      <A href="./">SAST 飞书登录</A>
+      <BackLayout className={[styles.layout]}>
+        <div className={`${styles.title}`}>{"<sast  link>"}</div>
+        <div className={`${styles.container}`}>
+          <Form
+            className={[`${styles.form}`]}
+            names={["username"]}
+            onSubmit={(args) => {
+              console.log(args);
+            }}
+          >
+            <Input className={[styles.input]} name={`username`} placeholder={`用户名或邮箱`} />
+            <Button className={[styles.formButton]} type={"submit"}>
+              登录
+            </Button>
+          </Form>
+          <A href="a" className={[styles.anchor]}>
+            SAST 飞书登录
+          </A>
+          <div className={`${styles.icons}`}>
+            <a>
+              <GithubIcon />{" "}
+            </a>
+            <a>
+              <QqIcon />
+            </a>
+            <a>
+              <MsIcon />
+            </a>
+          </div>
+          <div className={`${styles.toRegist}`}>
+            没有账号？<Link href={""}>注册</Link>
+          </div>
+        </div>
+      </BackLayout>
     </>
   );
 }
