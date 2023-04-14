d41850897037f8ea9ede28198efda19eb04a9dfd
Minngc <ming109152@gmail.com>
2023-04-14 21:22:56 +0800

    Background Layout

--- components/Layout/BackLayout/index.tsx
+++ components/Layout/BackLayout/index.tsx
@@ -1,17 +1,25 @@
 import styles from "./index.module.scss";
 import classNames from "classnames";
 
-const BackLayout = (props: any) => {
+interface BackLayoutProps {
+  type?: "green" | "orange" | "yellow" | "blue";
+}
+
+const BackLayout = (props: BackLayoutProps) => {
+  const { type = "yellow" } = props;
   return (
     <>
-      <div className={styles.bgLayout}>
-        <div className={styles.yellow}></div>
-
-        <div className={styles.blue}></div>
-
-        <div className={styles.orange}></div>
-
-        <div className={styles.green}></div>
+      <div className={`${styles.bgLayout} ${styles.pc}`}>
+        <div className={styles.blue} />
+        <div className={styles.yellow} />
+        <div className={styles.orange} />
+        <div className={styles.green} />
+      </div>
+      <div className={`${styles.mobile} ${styles.bgLayout}`}>
+        {type === "blue" && <div className={styles.blue} />}
+        {type === "yellow" && <div className={styles.yellow} />}
+        {type === "orange" && <div className={styles.orange} />}
+        {type === "green" && <div className={styles.green} />}
       </div>
     </>
   );
