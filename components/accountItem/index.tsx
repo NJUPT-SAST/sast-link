b66d5866520a63b8d1f51d8dbf749623efed8fbd
Minngc <ming109152@gmail.com>
2023-04-14 21:21:29 +0800

    accountItem

--- components/accountItem/index.tsx
+++ components/accountItem/index.tsx
@@ -1,19 +1,38 @@
 import { forwardRef } from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import styles from "./index.module.scss";
 import classNames from "classnames";
-import avator from "../../public/defaultAvator.png";
+import defaultAvator from "../../public/defaultAvator.png";
 import closeSVG from "../../public/svg/Vector.svg";
 
-const AccountItem = forwardRef(function Account(props: any, ref?) {
+interface AccountItemProps {
+  tabIndex: number;
+  nickName?: string;
+  mail?: string;
+  avator?: string | StaticImageData;
+}
+
+const AccountItem = forwardRef<any, AccountItemProps>(function Account(
+  props,
+  ref?,
+) {
+  const { tabIndex, nickName, mail, avator } = props;
+
   return (
-    <div className={`${styles.itemContainer}`}>
+    <div tabIndex={tabIndex} className={`${styles.itemContainer}`}>
       <div className={`${styles.avator}`}>
-        <Image src={avator} alt={`avator`} height={44} width={44} />
+        <Image
+          src={avator ?? defaultAvator}
+          alt={`avator`}
+          height={44}
+          width={44}
+        />
       </div>
       <div className={`${styles.accountMsg}`}>
-        <div className={`${styles.accountName}`}>Ming</div>
-        <div className={`${styles.accountMail}`}>B2100000@njupt.edu.cn</div>
+        <div className={`${styles.accountName}`}>{nickName ?? "Ming"}</div>
+        <div className={`${styles.accountMail}`}>
+          {mail ?? "B2100000@njupt.edu.cn"}
+        </div>
       </div>
       <div className={`${styles.closeIcon}`}>
         <Image src={closeSVG} alt={`close`} width={11} height={11} />
