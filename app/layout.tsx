c5bbbd7f59a0144ae6a6eb8ecdb2be30f4e68e76
Minngc <ming109152@gmail.com>
2023-07-06 11:56:58 +0800

    some files of app folder

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.scss";
+export const metadata = {
+  title: "SAST Link",
+  description: "OAuth of SAST",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="zh">
+      <body>{children}</body>
+    </html>
+  );
+}
