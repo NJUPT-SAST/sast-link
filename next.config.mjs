0958791d25c2e76f4dc899a9b617e668a3d91efe
Minngc <ming109152@gmail.com>
2023-07-10 19:19:58 +0800

    config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/apis/:slug*",
+        destination: `http://${process.env.DB_IP}:8080/api/v1//:slug*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
