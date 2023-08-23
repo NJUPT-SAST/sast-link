57bdd3b55ff18c0e83e62c05d3d550cdd8105855
Minngc <ming109152@gmail.com>
2023-08-23 17:15:39 +0800

    feat( ): auth page

--- app/(user)/auth/page.tsx
+++ app/(user)/auth/page.tsx
@@ -12,6 +12,7 @@ import useSWR from "swr";
 import { getUserInfo } from "@/lib/apis/user";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { oAuth } from "@/lib/apis/auth";
 
 const getMessage = async () => {
   if (!localStorage.getItem("Token"))
@@ -22,18 +23,19 @@ const getMessage = async () => {
 };
 
 export default function Auth() {
+  // TODO
+  const appName = "SAST Evento";
   const {
     data: { Success, Data },
   } = useSWR("infoUpdate", getMessage, { suspense: true });
   // 获取参数
-  console.log(Success);
   const router = useRouter();
   const searchParams = useSearchParams();
   const querys: {
     client_id: null | string;
     code_challenge: null | string;
     code_challenge_method: null | string;
-    redirect_url: null | string;
+    redirect_uri: null | string;
     response_type: null | string;
     scope: null | string;
     state: null | string;
@@ -41,7 +43,7 @@ export default function Auth() {
     client_id: null,
     code_challenge: null,
     code_challenge_method: null,
-    redirect_url: null,
+    redirect_uri: null,
     response_type: null,
     scope: null,
     state: null,
@@ -51,7 +53,7 @@ export default function Auth() {
     (querys as any)[key] = searchParams.get(key);
     return `${key}=${searchParams.get(key)}`;
   });
-  const redirect = `/auth?${querysArray.join("&")}`;
+  const redirect = `/auth?${JSON.stringify(querysArray)}`;
 
   useEffect(() => {
     if (!Success) router.push(`./login?redirect=${redirect}`);
@@ -77,7 +79,19 @@ export default function Auth() {
                   width={80}
                   height={80}
                 ></Image>
-                <div className={styles.username}>{"Ming"}</div>
+              </div>
+              <div className={styles.svgContainer}>
+                <svg
+                  aria-hidden="true"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="16"
+                  data-view-component="true"
+                  className={styles.svg}
+                >
+                  <path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path>
+                </svg>
               </div>
               <div className={styles.clientMessage}>
                 <Image
@@ -86,18 +100,27 @@ export default function Auth() {
                   width={80}
                   height={80}
                 ></Image>
-                <div className={styles.username}>{"SAST Evento"}</div>
               </div>
             </div>
-
-            {/* <div className={styles.email}>{Data.email}</div> */}
-            <ul className={styles.list}>
-              <li>获取用户头像、信息</li>
-              <li>获取其他信息</li>
-            </ul>
+            <div className={styles.authList}>
+              <div className={styles.authDescription}>
+                {
+                  // TODO 获取 appName 并指向争取的url
+                }
+                授权后，<a>{appName}</a> 将获得您的以下信息：
+              </div>
+              {/* <div className={styles.email}>{Data.email}</div> */}
+              <ul className={styles.list}>
+                <li>获取您的基本信息</li>
+                <li>获取账户的近期活动</li>
+                <li>获取其他信息</li>
+              </ul>
+            </div>
           </div>
           <Footer>
-            <Button>授权</Button>
+            <Button onClick={()=> {
+              oAuth(querys)
+            }}>授权</Button>
             <Anchor href={`/login?redirect=${redirect}`}>使用其他账号</Anchor>
           </Footer>
         </div>
