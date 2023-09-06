e01a00081a544b8897c2d4d486dfe725e2126bc3
MaxtuneLee <max@xox.im>
2023-09-06 11:04:55 +0800

    fix: null type issue

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -1,50 +1,53 @@
 "use client";
 
-import { useAppSelector } from "@/redux";
-import { useRouter } from "next/navigation";
-import { useAppDispatch } from "@/redux";
-import { login, logout } from "@/redux/features/userProfile";
+import {useRouter} from "next/navigation";
+import {useAppDispatch} from "@/redux";
+import {logout} from "@/redux/features/userProfile";
 import useSWR from "swr";
-import { getUserInfo } from "@/lib/apis/user";
-import { useEffect } from "react";
-import { removeAccount } from "@/redux/features/userList";
+import {getUserInfo} from "@/lib/apis/user";
+import {Suspense, useEffect} from "react";
+import {removeAccount} from "@/redux/features/userList";
 
 const getMessage = async () => {
-  if (!localStorage.getItem("Token"))
-    return { Success: false, Data: { username: "", email: "" } };
-  const info = await getUserInfo();
-  console.log(info);
-  return info.data;
+    if (!localStorage.getItem("Token"))
+        return {Success: false, Data: {username: "", email: ""}};
+    const info = await getUserInfo();
+    console.log(info);
+    return info.data;
 };
 
 const Home = () => {
-  const {
-    data: { Success, Data },
-  } = useSWR("infoUpdate", getMessage, { suspense: true });
-  const router = useRouter();
-  const dispatch = useAppDispatch();
-  const userProfile = useAppSelector((state) => state.currentUserProfile);
-  useEffect(() => {
-    if (!Success) {
-      router.replace("./");
-    }
-  }, [Success, router]);
-  return (
-    <>
-      Hello, {Data.email}, You have logged!
-      <div>
-        <button
-          onClick={() => {
-            router.replace("/");
-            dispatch(logout());
-            dispatch(removeAccount(Data.email));
-          }}
-        >
-          退出登錄
-        </button>
-      </div>
-    </>
-  );
+    const {
+        data: {Success, Data},
+    } = useSWR("infoUpdate", getMessage, {suspense: true});
+    const router = useRouter();
+    const dispatch = useAppDispatch();
+    // const userProfile = useAppSelector((state) => state.currentUserProfile);
+    useEffect(() => {
+        if (!Success) {
+            router.replace("./");
+        }
+    }, [Success, router]);
+    return (
+        <Suspense fallback={<h1>数据加载中...</h1>}>
+            <>
+                Hello, {Data?.email}, You have logged!
+                <div>
+                    <button
+                        onClick={() => {
+                            if (Data !== null) {
+                                router.replace("/");
+                                dispatch(logout());
+                                dispatch(removeAccount(Data.email));
+                            }
+                        }}
+                    >
+                        退出登錄
+                    </button>
+                </div>
+            </>
+        </Suspense>
+    );
 };
 
 export default Home;
--- components/veriCode/index.tsx
+++ components/veriCode/index.tsx
@@ -1,60 +1,61 @@
-import { useContext, useState, useEffect } from "react";
+import {useContext, useState, useEffect} from "react";
 import classNames from "classnames";
 import styles from "./index.module.scss";
-import { RegistContext } from "@/lib/context";
-import { sendMail, veriRegistAccount } from "@/lib/apis/global";
+import {RegistContext} from "@/lib/context";
+import {sendMail, veriRegistAccount} from "@/lib/apis/global";
 
 const VeriCode = () => {
-  const [clickable, setClickable] = useState<boolean>(false);
-  const [count, setCount] = useState<number>(60);
-  const { username = "",handleTicket } = useContext(RegistContext);
+    const [clickable, setClickable] = useState<boolean>(false);
+    const [count, setCount] = useState<number>(60);
+    const {username = "", handleTicket} = useContext(RegistContext);
 
-  useEffect(() => {
-    let intervalId: NodeJS.Timer;
-    if (clickable === false) {
-      intervalId = setInterval(() => {
-        setCount((pre) => {
-          if (pre <= 0) {
-            setClickable(true);
-            clearInterval(intervalId);
-            return 60;
-          }
-          return pre - 1;
-        });
-      }, 1000);
-    }
-    return () => clearInterval(intervalId);
-  }, [clickable]);
+    useEffect(() => {
+        let intervalId: NodeJS.Timer;
+        if (!clickable) {
+            intervalId = setInterval(() => {
+                setCount((pre) => {
+                    if (pre <= 0) {
+                        setClickable(true);
+                        clearInterval(intervalId);
+                        return 60;
+                    }
+                    return pre - 1;
+                });
+            }, 1000);
+        }
+        return () => clearInterval(intervalId);
+    }, [clickable]);
 
-  return (
-    <>
+    return (
+        <>
       <span
-        aria-disabled={clickable}
-        onClick={() => {
-          setClickable(false);
-          console.log(username);
-          veriRegistAccount(username)
-            .then(
-              (res) => {
-                console.log(res.data.Data.register_ticket);
-                const ticket  = res.data.Data.register_ticket
-                handleTicket(ticket)
-                return sendMail(ticket);
-              },
-              (err) => console.log(err)
-            )
-            .then((res) => console.log(res))
-            .catch((err) => console.log(err));
-        }}
-        className={` ${styles.veriCode}
+          aria-disabled={clickable}
+          onClick={() => {
+              setClickable(false);
+              console.log(username);
+              veriRegistAccount(username)
+                  .then(
+                      (res) => {
+                          if (res.data.Success) {
+                              const ticket = res.data.Data.register_ticket
+                              handleTicket(ticket)
+                              return sendMail(ticket);
+                          }
+                      },
+                      (err) => console.log(err)
+                  )
+                  .then((res) => console.log(res))
+                  .catch((err) => console.log(err));
+          }}
+          className={` ${styles.veriCode}
          ${classNames({
-           [styles.clickable]: clickable,
-         })}`}
+              [styles.clickable]: clickable,
+          })}`}
       >
         {clickable ? "" : `${count}s 后`}重新发送
       </span>
-    </>
-  );
+        </>
+    );
 };
 
-export { VeriCode };
+export {VeriCode};
