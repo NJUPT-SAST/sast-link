f02b76d065254ab46501a9ebe22da79e222da662
Minngc <ming109152@gmail.com>
2023-10-07 14:15:05 +0800

    home panel

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 import { getUserInfo } from "@/lib/apis/user";
 import { Suspense } from "react";
 import classNames from "classnames";
+
 import styles from "./page.module.scss";
 import avatar from "@public/defaultAvator.png";
 import {
@@ -33,52 +34,56 @@ const Home = () => {
   const router = useRouter();
   return (
     <Suspense fallback={<h1>数据加载中...</h1>}>
-      <div className={classNames(styles.profileCard)}>
-        <div className={classNames(styles.messageSide)}>
-          <div className={classNames(styles.avatar_large)}>
-            <Icon src={avatar} alt={"user avatar"} width={140} height={140} />
-          </div>
-          <div className={classNames(styles.userBasicMessage)}>
-            <div className={classNames(styles.userName)}>Ming</div>
-            <div className={classNames(styles.userEmail)}>Ming123@456.com</div>
-          </div>
+      <div className={styles.leftPanel}>
+        <div className={classNames(styles.profileCard)}>
+          <div className={classNames(styles.messageSide)}>
+            <div className={classNames(styles.avatar_large)}>
+              <Icon src={avatar} alt={"user avatar"} width={140} height={140} />
+            </div>
+            <div className={classNames(styles.userBasicMessage)}>
+              <div className={classNames(styles.userName)}>Ming</div>
+              <div className={classNames(styles.userEmail)}>
+                Ming123@456.com
+              </div>
+            </div>
 
-          <div className={classNames(styles.userPosition)}>
-            软件研发中心 老东西
+            <div className={classNames(styles.userPosition)}>
+              软件研发中心 老东西
+            </div>
+          </div>
+          <div className={classNames(styles.linkSide)}>
+            <Link className={classNames(styles.toProfile)} href={"./home"}>
+              <span>个人主页</span>
+              <Icon {...blackArrowheadIcon} />
+            </Link>
           </div>
         </div>
-        <div className={classNames(styles.linkSide)}>
-          <Link className={classNames(styles.toProfile)} href={"./home"}>
-            <span>个人主页</span>
-            <Icon {...blackArrowheadIcon} />
-          </Link>
+
+        <div className={classNames(styles.bindPanel)}>
+          <BindAppItem
+            bindAppIconProps={qqIcon}
+            bindAppTitle={"QQ"}
+            binded={false}
+          />
+
+          <BindAppItem
+            bindAppIconProps={feishuIcon}
+            bindAppTitle={"SAST 飞书"}
+            binded={false}
+          />
+
+          <BindAppItem
+            bindAppIconProps={msIcon}
+            bindAppTitle={"Microsoft"}
+            binded={false}
+          />
+
+          <BindAppItem
+            bindAppIconProps={githubIcon}
+            bindAppTitle={"GutHub"}
+            binded={false}
+          />
         </div>
-      </div>
-
-      <div className={classNames(styles.bindPanel)}>
-        <BindAppItem
-          bindAppIconProps={qqIcon}
-          bindAppTitle={"QQ"}
-          binded={false}
-        />
-
-        <BindAppItem
-          bindAppIconProps={feishuIcon}
-          bindAppTitle={"SAST 飞书"}
-          binded={false}
-        />
-
-        <BindAppItem
-          bindAppIconProps={msIcon}
-          bindAppTitle={"Microsoft"}
-          binded={false}
-        />
-
-        <BindAppItem
-          bindAppIconProps={githubIcon}
-          bindAppTitle={"GutHub"}
-          binded={false}
-        />
       </div>
     </Suspense>
   );
