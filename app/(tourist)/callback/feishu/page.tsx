af0ec426b4492ef32e74c5b9ce11f4f7b9860e0f
MaxtuneLee <max@xox.im>
2024-07-16 14:48:55 +0800

    feat: callback 动画

--- app/(tourist)/callback/feishu/page.tsx
+++ app/(tourist)/callback/feishu/page.tsx
@@ -6,6 +6,11 @@ import { useAppDispatch } from "@/redux";
 import { addAccount } from "@/redux/features/userList";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import styles from "./page.module.scss";
+import { cupSaucer } from "@lucide/lab";
+import { Icon } from "lucide-react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import BackLayout from "@/components/Layout/BackLayout";
 
 const FeishuCallback = ({
   searchParams,
@@ -23,7 +28,6 @@ const FeishuCallback = ({
 
   useEffect(() => {
     getFeishuLoginStatus(code, state).then((feishuRes) => {
-      console.log(feishuRes);
       if (feishuRes.data.Success) {
         localStorage.setItem(
           "Token",
@@ -48,16 +52,27 @@ const FeishuCallback = ({
         if (feishuRes.data.ErrCode === 50000) {
           return <div>飞书登录失败</div>;
         }
-        console.log("跳转");
         router.replace(`/login?oauthTicket=${feishuRes.data.Data.oauthTicket}`);
       }
     });
   }, [code, dispatch, router, state]);
 
   return (
-    <>
-      <div>正在重定向</div>
-    </>
+    <div className={styles["base"]}>
+      <BackLayout type="green" />
+      <div className={styles["wrap"]}>
+        {/* <Icon iconNode={cupSaucer} size={48} strokeWidth={1.25} /> */}
+        <Player
+          autoplay={true}
+          loop={false}
+          src="/svg/done.json"
+          style={{ height: "64px", width: "64px" }}
+          keepLastFrame
+          renderer="svg"
+        />
+        <div className={styles["text"]}>授权成功，正在重定向</div>
+      </div>
+    </div>
   );
 };
 
