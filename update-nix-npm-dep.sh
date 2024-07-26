d6ec0ed7e66481f739a7594d098dfd86e5d6f1af
aimixsaka <aimixsaka@gmail.com>
2024-07-26 15:13:05 +0800

    fix(nix ci): action can only use one `uses` in one step

--- update-nix-npm-dep.sh
+++ update-nix-npm-dep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # script help to update package-lock.json and nix flake npmDepsHash
-npm i --package-lock-only
+nix shell nixpkgs#nodePackages.npm nixpkgs#husky --command npm i --package-lock-only
 sed -i "s|npmDepsHash = \".*\";|npmDepsHash = \"$(nix run nixpkgs#prefetch-npm-deps package-lock.json | tail -1)\";|" flake.nix
