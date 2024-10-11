2f9b651bfdfdfd828541494b8c28cb259d70b5e7
aimixsaka <aimixsaka@gmail.com>
2024-10-11 23:38:31 +0800

    fix(nix): add missing `package-lock.json

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           buildInputs  = with pkgs; [
             nodePackages.node-gyp-build
           ];
-          npmDepsHash = "sha256-Qx/VrcHJZEhK9FLjJEMYk5U+9C9ok11lOcy4kPx+7ME="; # <prefetch-npm-deps package-lock.json>
+          npmDepsHash = "sha256-8x/IcpOSgrZG4fXX+o/wtXx7tr7EBIfOVg/O2TM3Jxo="; # <prefetch-npm-deps package-lock.json>
 
           # see ./next.config.mjs
           env.OUTPUT_STANDALONE = true;
