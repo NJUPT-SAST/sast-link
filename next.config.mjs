995231096a09c702eb03548497c002ae94f38d2d
MaxtuneLee <max@xox.im>
2024-10-11 14:15:10 +0800

    ci: backend update

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/apis/:slug*",
-        destination: `http://81.68.225.220:8080/api/v1/:slug*`,
+        destination: `http://81.68.225.220:8081/api/v1/:slug*`,
       },
     ];
   },
